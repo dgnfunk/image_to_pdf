9300ad348552312bcf5d9fac82e980a1c3117443
dario <ruben.g1293@gmail.com>
2022-11-20 22:46:05 -0600

    add some asserts validations

--- src/main.py
+++ src/main.py
@@ -1,21 +1,35 @@
-from PIL import Image
 import glob
 import os
 import sys
 
+from PIL import Image
+
+
 def get_image_names(path):
-    isDirectory = os.path.isdir(path)
-    if isDirectory is False:
-        return []
-    # This is my path
-    path_wildcard = "\\*.png"
-    return glob.iglob(path + path_wildcard)
+    assert os.path.isdir(path), 'Source is not a valid folder'
+    path_wildcard = "*.png"
+    full_path_wildcard = os.path.join(path, path_wildcard)
+    return glob.iglob(full_path_wildcard)
+
 
 def convertToPdf(name, path_src, path_dst):
     file_names = get_image_names(path_src)
     images = [Image.open(file_path) for file_path in file_names]
+    assert len(images) > 0, 'No image to be converted to PDF.'
     first_image = images[0]
-    first_image.save(path_dst + '\\' + name, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
+    assert os.path.isdir(path_dst), 'Dest is not a valid Folder.'
+    save_destination = os.path.join(path_dst, name)
+    first_image.save(
+        save_destination,
+        "PDF",
+        resolution=100.0,
+        save_all=True,
+        append_images=images[1:]
+    )
+
 
 if __name__ == '__main__':
+    assert len(sys.argv) >= 2, 'Name might be defined to continue.'
+    assert len(sys.argv) >= 3, 'Source might be defined to continue.'
+    assert len(sys.argv) >= 4, 'Destination might be defined to continue.'
     convertToPdf(sys.argv[1], sys.argv[2], sys.argv[3])
